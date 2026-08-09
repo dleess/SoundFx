1dc5b6d28739108a6ba995a82f0c2f02485a239b
dleess <92695827+dleess@users.noreply.github.com>
2026-08-09 15:44:07 +0900

    Release v1.0: bundle id com.donghan.soundfx, Safari manifest description <=112 chars (#4)
    Co-authored-by: Donghan Lee <lee.donghan@gmail.com>

--- safari/SoundFx/Shared (App)/ViewController.swift
+++ safari/SoundFx/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "com.donghan.sound.dev.Extension"
+let extensionBundleIdentifier = "com.donghan.soundfx.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
